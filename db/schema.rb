52500a8ab33b0f63a72b0fbe2cc10ec4747ae4e0
Evans Kofi Nyamekye <nyamekye2131@gmail.com>
2024-02-14 10:57:07 +0000

    Update: Modify database & schema profile

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_02_12_220120) do
+ActiveRecord::Schema[7.1].define(version: 2023_12_28_132311) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -23,13 +23,24 @@ ActiveRecord::Schema[7.1].define(version: 2024_02_12_220120) do
     t.index ["user_id"], name: "index_categories_on_user_id"
   end
 
+  create_table "categories_expenses", force: :cascade do |t|
+    t.bigint "category_id", null: false
+    t.bigint "expense_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["category_id"], name: "index_categories_expenses_on_category_id"
+    t.index ["expense_id"], name: "index_categories_expenses_on_expense_id"
+  end
+
   create_table "expenses", force: :cascade do |t|
     t.string "name"
-    t.string "account"
+    t.decimal "amount"
     t.bigint "author_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "category_id", null: false
     t.index ["author_id"], name: "index_expenses_on_author_id"
+    t.index ["category_id"], name: "index_expenses_on_category_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -46,5 +57,8 @@ ActiveRecord::Schema[7.1].define(version: 2024_02_12_220120) do
   end
 
   add_foreign_key "categories", "users"
+  add_foreign_key "categories_expenses", "categories"
+  add_foreign_key "categories_expenses", "expenses"
+  add_foreign_key "expenses", "categories"
   add_foreign_key "expenses", "users", column: "author_id"
 end
