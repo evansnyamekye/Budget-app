fc6b505c4560cd1a7da6d6f2b241ccea96c857b1
Evans Kofi Nyamekye <nyamekye2131@gmail.com>
2024-02-16 23:00:25 +0000

    Update: modify database and schema

--- db/migrate/20231223155230_add_devise_to_users.rb
+++ db/migrate/20231223155230_add_devise_to_users.rb
@@ -35,8 +35,6 @@ class AddDeviseToUsers < ActiveRecord::Migration[7.1]
 
       # Uncomment below if timestamps were not included in your original model.
       # t.timestamps null: false
-      t.string :name
-      t.string :image
     end
 
     add_index :users, :email,                unique: true
