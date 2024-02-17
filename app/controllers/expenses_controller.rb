70303b03ec86b3274f26d02e28204c93f2f3ec4e
Evans Kofi Nyamekye <nyamekye2131@gmail.com>
2024-02-17 19:36:48 +0000

    Update: environments configuration files updated

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -1,34 +1,42 @@
 class ExpensesController < ApplicationController
   def index
-    @user = User.find(params[:user_id])
     @category = Category.find(params[:category_id])
-    @expenses = @user.expenses.includes(:category).where(category: @category)
+    @expenses = @category.expenses.order(created_at: :desc)
   end
 
   def new
-    @user = User.find(params[:user_id])
     @category = Category.find(params[:category_id])
-    @expense = Expense.new
-    @categories = Category.all
+    @expense = @category.expenses.build
   end
 
   def create
-    @user = User.find(params[:user_id])
-    @category = Category.find(params[:expense][:category_id])
+    @category = Category.find(params[:category_id])
     @expense = @category.expenses.build(expense_params)
-    @expense.author = @user
+    @expense.author = current_user
 
-    if @expense.save
-      redirect_to user_category_expenses_path(@user, @category), notice: 'Expense was successfully created.'
-    else
-      @categories = Category.all
-      render :new
+    category_id = params[:expense][:category_id]
+
+    if category_id.present?
+      category = Category.find(category_id)
+      @expense.categories << category
+    end
+
+    respond_to do |format|
+      if @expense.save!
+        format.html do
+          redirect_to category_expenses_path(category_id), notice: 'Transaction created successfully!'
+        end
+        format.json { render :show, status: :created, location: @expense }
+      else
+        format.html { render :new, flash: { error: 'Transaction not created!' } }
+        format.json { render json: @expense.errors, status: :unprocessable_entity }
+      end
     end
   end
 
   private
 
   def expense_params
-    params.require(:expense).permit(:name, :amount, :transaction_date, :category_id)
+    params.require(:expense).permit(:name, :amount)
   end
 end
