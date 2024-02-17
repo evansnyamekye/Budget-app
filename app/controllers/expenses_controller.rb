4d751ca1a06df2fd7876c66d70c37d0e725777a7
Evans Kofi Nyamekye <nyamekye2131@gmail.com>
2024-02-17 19:30:45 +0000

    expenses files updated

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -1,42 +1,34 @@
 class ExpensesController < ApplicationController
   def index
+    @user = User.find(params[:user_id])
     @category = Category.find(params[:category_id])
-    @expenses = @category.expenses.order(created_at: :desc)
+    @expenses = @user.expenses.includes(:category).where(category: @category)
   end
 
   def new
+    @user = User.find(params[:user_id])
     @category = Category.find(params[:category_id])
-    @expense = @category.expenses.build
+    @expense = Expense.new
+    @categories = Category.all
   end
 
   def create
-    @category = Category.find(params[:category_id])
+    @user = User.find(params[:user_id])
+    @category = Category.find(params[:expense][:category_id])
     @expense = @category.expenses.build(expense_params)
-    @expense.author = current_user
+    @expense.author = @user
 
-    category_id = params[:expense][:category_id]
-
-    if category_id.present?
-      category = Category.find(category_id)
-      @expense.categories << category
-    end
-
-    respond_to do |format|
-      if @expense.save!
-        format.html do
-          redirect_to category_expenses_path(category_id), notice: 'Transaction created successfully!'
-        end
-        format.json { render :show, status: :created, location: @expense }
-      else
-        format.html { render :new, flash: { error: 'Transaction not created!' } }
-        format.json { render json: @expense.errors, status: :unprocessable_entity }
-      end
+    if @expense.save
+      redirect_to user_category_expenses_path(@user, @category), notice: 'Expense was successfully created.'
+    else
+      @categories = Category.all
+      render :new
     end
   end
 
   private
 
   def expense_params
-    params.require(:expense).permit(:name, :amount)
+    params.require(:expense).permit(:name, :amount, :transaction_date, :category_id)
   end
 end
